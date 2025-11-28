b54a2a6bb2bc32d58e03b75f0bc79070f26e33cc
Akum6x <dfrale@mail.ru>
2025-11-28 20:58:37 +0300

    Update Green.cs

--- Lab5/Green.cs
+++ Lab5/Green.cs
@@ -17,19 +17,19 @@ namespace Lab5
             for (int i = 0; i < rows; i++)
             {
                 int mdex = 0;
-                int minval = matrix[i,0];
+                int minval = matrix[i, 0];
 
                 for (int j = 0; j < cols; j++)
                 {
-                    if (matrix[i,j] < minval)
+                    if (matrix[i, j] < minval)
                     {
-                        minval = matrix[i,j];
+                        minval = matrix[i, j];
                         mdex = j;
                     }
                 }
                 answer[i] = mdex;
             }
-            
+
             // end
 
             return answer;
@@ -46,19 +46,19 @@ namespace Lab5
                 int maxiex = 0;
                 int maxval = matrix[i, 0];
 
-                for (int j = 0; j < cols;j++)
+                for (int j = 0; j < cols; j++)
                 {
-                    if (matrix[i,j] > maxval)
+                    if (matrix[i, j] > maxval)
                     {
-                        maxval = matrix[i,j];
+                        maxval = matrix[i, j];
                         maxiex = j;
                     }
                 }
                 for (int j = 0; j < maxiex; j++)
                 {
-                    if (matrix[i,j] < 0)
+                    if (matrix[i, j] < 0)
                     {
-                        matrix[i,j] = (int)Math.Floor((double)matrix[i,j] / maxval);
+                        matrix[i, j] = (int)Math.Floor((double)matrix[i, j] / maxval);
                     }
                 }
             }
@@ -83,19 +83,19 @@ namespace Lab5
 
             int maxDcol = 0;
             int maxDval = matrix[0, 0];
-            
+
             for (int i = 1; i < size; i++)
             {
-                if (matrix[i,i] >  maxDval)
+                if (matrix[i, i] > maxDval)
                 {
-                    maxDval = matrix[i,i];
+                    maxDval = matrix[i, i];
                     maxDcol = i;
                 }
             }
             if (maxDcol == k)
             {
                 return;
-            }    
+            }
             for (int i = 0; i < size; i++)
             {
                 int temp = matrix[i, k];
@@ -109,7 +109,52 @@ namespace Lab5
         {
 
             // code here
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
 
+            if (rows == 1)
+            {
+                matrix = new int[0, cols];
+                return;
+            }
+
+            int maxSum = -1;
+            int rowToDelete = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                int currentSum = 0;
+                for (int j = 0; j < cols; j++)
+                {
+                    if (matrix[i, j] > 0)
+                    {
+                        currentSum += matrix[i, j];
+                    }
+                }
+
+                if (currentSum > maxSum)
+                {
+                    maxSum = currentSum;
+                    rowToDelete = i;
+                }
+            }
+
+            int[,] newMatrix = new int[rows - 1, cols];
+            int newRow = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                if (i != rowToDelete)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        newMatrix[newRow, j] = matrix[i, j];
+                    }
+                    newRow++;
+                }
+            }
+
+            matrix = newMatrix;
             // end
 
         }
@@ -118,7 +163,49 @@ namespace Lab5
             int[,] answer = null;
 
             // code here
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
 
+            if (rows <= 1)
+            {
+                answer = new int[0, cols];
+                return answer;
+            }
+
+            int maxSum = -1;
+            int rowToDelete = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                int currentSum = 0;
+                for (int j = 0; j < cols; j++)
+                {
+                    if (matrix[i, j] > 0)
+                    {
+                        currentSum += matrix[i, j];
+                    }
+                }
+                if (currentSum > maxSum)
+                {
+                    maxSum = currentSum;
+                    rowToDelete = i;
+                }
+            }
+
+            answer = new int[rows - 1, cols];
+            int newRow = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                if (i != rowToDelete)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        answer[newRow, j] = matrix[i, j];
+                    }
+                    newRow++;
+                }
+            }
             // end
 
             return answer;
@@ -127,7 +214,54 @@ namespace Lab5
         {
 
             // code here
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
 
+            if (rows < 2) return;
+
+            int[] negativeCounts = new int[rows];
+
+            for (int i = 0; i < rows; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < cols; j++)
+                {
+                    if (matrix[i, j] < 0)
+                    {
+                        count++;
+                    }
+                }
+                negativeCounts[i] = count;
+            }
+
+            int minCount = negativeCounts[0];
+            int maxCount = negativeCounts[0];
+            int minRowIndex = 0;
+            int maxRowIndex = 0;
+
+            for (int i = 1; i < rows; i++)
+            {
+                if (negativeCounts[i] < minCount)
+                {
+                    minCount = negativeCounts[i];
+                    minRowIndex = i;
+                }
+
+                if (negativeCounts[i] > maxCount)
+                {
+                    maxCount = negativeCounts[i];
+                    maxRowIndex = i;
+                }
+            }
+
+            if (minCount == maxCount) return;
+
+            for (int j = 0; j < cols; j++)
+            {
+                int temp = matrix[minRowIndex, j];
+                matrix[minRowIndex, j] = matrix[maxRowIndex, j];
+                matrix[maxRowIndex, j] = temp;
+            }
             // end
 
         }
@@ -136,7 +270,45 @@ namespace Lab5
             int[,] answer = null;
 
             // code here
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
 
+            if (array.Length != rows)
+            {
+                return matrix;
+            }
+
+            int minValue = matrix[0, 0];
+            int minCol = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (matrix[i, j] < minValue)
+                    {
+                        minValue = matrix[i, j];
+                        minCol = j;
+                    }
+                }
+            }
+
+            answer = new int[rows, cols + 1];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j <= minCol; j++)
+                {
+                    answer[i, j] = matrix[i, j];
+                }
+
+                answer[i, minCol + 1] = array[i];
+
+                for (int j = minCol + 1; j < cols; j++)
+                {
+                    answer[i, j + 1] = matrix[i, j];
+                }
+            }
             // end
 
             return answer;
@@ -145,7 +317,37 @@ namespace Lab5
         {
 
             // code here
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
 
+            for (int j = 0; j < cols; j++)
+            {
+                int positiveCount = 0;
+                int negativeCount = 0;
+                int maxValue = matrix[0, j];
+                int maxRowIndex = 0;
+
+                for (int i = 0; i < rows; i++)
+                {
+                    if (matrix[i, j] > 0) positiveCount++;
+                    else if (matrix[i, j] < 0) negativeCount++;
+
+                    if (matrix[i, j] > maxValue)
+                    {
+                        maxValue = matrix[i, j];
+                        maxRowIndex = i;
+                    }
+                }
+
+                if (positiveCount > negativeCount)
+                {
+                    matrix[maxRowIndex, j] = 0;
+                }
+                else if (negativeCount > positiveCount)
+                {
+                    matrix[maxRowIndex, j] = maxRowIndex;
+                }
+            }
             // end
 
         }
@@ -153,7 +355,18 @@ namespace Lab5
         {
 
             // code here
+            int n = matrix.GetLength(0);
 
+            for (int k = 0; k < n * n; k++)
+            {
+                int i = k / n;
+                int j = k % n;
+
+                if (i == 0 || i == n - 1 || j == 0 || j == n - 1)
+                {
+                    matrix[i, j] = 0;
+                }
+            }
             // end
 
         }
@@ -162,26 +375,60 @@ namespace Lab5
             int[] A = null, B = null;
 
             // code here
+            int n = matrix.GetLength(0);
 
+            int sizeA = n * (n + 1) / 2;
+            int sizeB = n * (n - 1) / 2;
+
+            A = new int[sizeA];
+            B = new int[sizeB];
+
+            int ia = 0, ib = 0;
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    if (j >= i)
+                        A[ia++] = matrix[i, j];
+                    else
+                        B[ib++] = matrix[i, j];
             // end
 
             return (A, B);
-        }
-        public void Task11(int[,] matrix)
-        {
-
-            // code here
-
-            // end
-
         }
         public void Task12(int[][] array)
         {
 
             // code here
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    bool shouldSwap = false;
 
+                    if (array[j].Length > array[i].Length)
+                    {
+                        shouldSwap = true;
+                    }
+                    else if (array[j].Length == array[i].Length)
+                    {
+                        int sumI = array[i].Sum();
+                        int sumJ = array[j].Sum();
+
+                        if (sumJ > sumI)
+                        {
+                            shouldSwap = true;
+                        }
+                    }
+
+                    if (shouldSwap)
+                    {
+                        int[] temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
             // end
-
         }
     }
 }
