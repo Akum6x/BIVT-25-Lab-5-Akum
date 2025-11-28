7f1c065676cf6f26f1fdda641509d96764bdc0f1
Akum6x <dfrale@mail.ru>
2025-11-28 15:34:13 +0300

    Update Green.cs

--- Lab5/Green.cs
+++ Lab5/Green.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+using System.Linq;
 using System.Runtime.InteropServices;
 
 namespace Lab5
@@ -10,7 +10,26 @@ namespace Lab5
             int[] answer = null;
 
             // code here
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
 
+            answer = new int[rows];
+            for (int i = 0; i < rows; i++)
+            {
+                int mdex = 0;
+                int minval = matrix[i,0];
+
+                for (int j = 0; j < cols; j++)
+                {
+                    if (matrix[i,j] < minval)
+                    {
+                        minval = matrix[i,j];
+                        mdex = j;
+                    }
+                }
+                answer[i] = mdex;
+            }
+            
             // end
 
             return answer;
@@ -19,7 +38,30 @@ namespace Lab5
         {
 
             // code here
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
 
+            for (int i = 0; i < rows; i++)
+            {
+                int maxiex = 0;
+                int maxval = matrix[i, 0];
+
+                for (int j = 0; j < cols;j++)
+                {
+                    if (matrix[i,j] > maxval)
+                    {
+                        maxval = matrix[i,j];
+                        maxiex = j;
+                    }
+                }
+                for (int j = 0; j < maxiex; j++)
+                {
+                    if (matrix[i,j] < 0)
+                    {
+                        matrix[i,j] = (int)Math.Floor((double)matrix[i,j] / maxval);
+                    }
+                }
+            }
             // end
 
         }
@@ -27,7 +69,39 @@ namespace Lab5
         {
 
             // code here
+            if (matrix == null)
+            {
+                return;
+            }
 
+            int size = matrix.GetLength(0);
+
+            if (size != matrix.GetLength(1) || k < 0 || k >= size)
+            {
+                return;
+            }
+
+            int maxDcol = 0;
+            int maxDval = matrix[0, 0];
+            
+            for (int i = 1; i < size; i++)
+            {
+                if (matrix[i,i] >  maxDval)
+                {
+                    maxDval = matrix[i,i];
+                    maxDcol = i;
+                }
+            }
+            if (maxDcol == k)
+            {
+                return;
+            }    
+            for (int i = 0; i < size; i++)
+            {
+                int temp = matrix[i, k];
+                matrix[i, k] = matrix[i, maxDcol];
+                matrix[i, maxDcol] = temp;
+            }
             // end
 
         }
